eedec5531a211763c5538e5eee907928ca9d5693
Ivan Georgiev <ivan_georgiew@yahoo.com>
2017-05-14 18:53:39 +0300

    yet another heroku fix attempts

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import { reducer } from './reducer';
 
 
 /* EXPRESS SETUP */
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const publicPath = path.join(__dirname, '../public');
 
